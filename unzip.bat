7c47d468740058d414b6bc22d4db28a4a4b74f33
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-08-14 03:40:54 -0400

    Update unzip.bat

--- unzip.bat
+++ unzip.bat
@@ -7,17 +7,7 @@ TITLE RECURSIVELY UNZIP
 
 PUSHD "%~dp0"
 
-ECHO CD: "%~dp0"
-ECHO=
-PAUSE
-GOTO :EOF
-
 :----------------------------------------------------------------------------------------------
 
-FOR /F "USEBACKQ TOKENS=*" %%G IN (`DIR /S /B *.zip`) DO ECHO 7z.exe x "%%G"
-
-:----------------------------------------------------------------------------------------------
-
-ECHO=
-PAUSE
-REM TIMEOUT 3 >NUL
+FOR /F "USEBACKQ TOKENS=*" %%G IN (`DIR /S /B *.zip`) DO 7z.exe x -y "%%G" -o"%%~dpnG"
+TIMEOUT 3 >NUL
