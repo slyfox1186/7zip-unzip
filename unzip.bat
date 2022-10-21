7cfc3f7aadbb8c62d79eb77c9a16c738c7d69a49
Jeff <37704019+slyfox1186@users.noreply.github.com>
2022-10-21 10:29:03 -0400

    Update unzip.bat

--- unzip.bat
+++ unzip.bat
@@ -1,13 +1,32 @@
 @ECHO OFF
 SETLOCAL ENABLEEXTENSIONS
 COLOR 0A
-TITLE Recursively unzip zip archives
+TITLE Recursively extract zip archives
+
+:--------------------------------------------------------------------------------
 
 PUSHD "%~dp0"
+IF NOT "%1"=="MAX" START /MAX CMD /D /C %0 MAX & GOTO :EOF
+
+:--------------------------------------------------------------------------------
+
+REM YOU CAN SET THE FULL PATH OF 7z.exe HERE IF YOU WISH.
+REM IF YOU DECIDE TO USE THIS VARIABLE YOU MUST REPLACE 7z.exe
+REM IN THE BELOW COMMAND WITH %SZIP%
+
+SET SZIP="C:\Program Files\7-Zip\7z.exe"
+
+:--------------------------------------------------------------------------------
+
+REM RECURSIVELY SEARCH FOR ALL ZIP FILES AND EXTRACT THEIR
+REM CONTENTS TO A FOLDER WITH THE SAME NAME AS THE ARCHIVE.
+REM UNCOMMENT DEL /Q "%%G" BELOW TO DELETE THE ARCHIVES AFTER EXTRACTION
 
 FOR /F "USEBACKQ TOKENS=*" %%G IN (`DIR /S /B *.zip`) DO (
     7z.exe x -y "%%G" -o"%%~dpnG"
-    DEL /Q "%%G"
+    REM DEL /Q "%%G"
 )
+
+:--------------------------------------------------------------------------------
 
 TIMEOUT 1 >NUL
